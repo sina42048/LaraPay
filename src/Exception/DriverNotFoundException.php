9062c4ab3c4557091b08d56ca01f45b538f77e84
sina42048 <sinafat76@gmail.com>
2021-05-25 08:58:43 +0430

    exception files

---
+++ src/Exception/DriverNotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+namespace Sina42048\LaraPay\Exception;
+
+use Exception;
+
+/**
+ * class DriverNotFoundException
+ * @author Sina Fathollahi
+ * @package Sina42048\LaraPay\Exception
+ */
+class DriverNotFoundException extends Exception {
+
+}
---
+++ src/Exception/PaymentRequestException.php
@@ -0,0 +1,13 @@
+<?php
+namespace Sina42048\LaraPay\Exception;
+
+use Exception;
+
+/**
+ * class PaymentRequestException
+ * @author Sina Fathollahi
+ * @package Sina42048\LaraPay\Exception
+ */
+class PaymentRequestException extends Exception {
+
+}
