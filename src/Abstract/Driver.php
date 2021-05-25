49b48d927efa59e155557ffd2cdc449ef3122430
sina42048 <sinafat76@gmail.com>
2021-05-25 08:57:54 +0430

    abstract class that all drivers should extend it

---
+++ src/Abstract/Driver.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Sina42048\LaraPay\Abstract;
+
+use \GuzzleHttp\Client;
+
+/**
+ * class Driver
+ * @author Sina Fathollahi
+ * @package Sina42048\LaraPay\Abstract
+ */
+abstract class Driver {
+
+    /**
+     * hold specified driver config
+     * @var array $config 
+     */
+    protected $config;
+
+    /**
+     * hold LaraBill class data
+     * @var \Sina42048\LaraPay\LaraBill $bill
+     */
+    protected $bill;
+
+    /**
+     * guzzle http client instance
+     * @var \GuzzleHttp\Client $client
+     */
+    protected $client;
+
+    /**
+     * @param string $config specified driver config
+     * @param \Sina42048\LaraPay\LaraBill $bill bill instance
+     * @return void
+     */
+    public function __construct(array $config, \Sina42048\LaraPay\LaraBill $bill) {
+        $this->config = $config;
+        $this->bill = $bill;
+        $this->client = new Client();
+    }
+
+    /**
+     * request payment to web service
+     * @throws \Sina42048\LaraPay\Exception\PaymentRequestException
+     */
+    public abstract function pay();
+
+    /**
+     * translate messages that web service api retrive
+     * @param int $statusCode error status code
+     * @param int $errorCode error code
+     * @return string
+     */
+    protected abstract function translateErrorMessages($statusCode, $errorCode);
+}
