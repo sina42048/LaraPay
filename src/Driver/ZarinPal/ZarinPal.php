f7a948370efab3e49ec0deb4cef3f9cfd6ec542d
sina42048 <sinafat76@gmail.com>
2021-05-25 12:25:26 +0430

    added drivers todo

---
+++ src/Driver/ZarinPal/ZarinPal.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Sina42048\LaraPay\Driver\ZarinPal;
+
+use Sina42048\LaraPay\Abstract\Driver;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\View;
+use Sina42048\LaraPay\Exception\PaymentRequestException;
+
+/**
+ * class IdPay
+ * @author Sina Fathollahi
+ * @package Sina42048\LaraPay\Driver
+ */
+class ZarinPal extends Driver{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function pay() {
+        // TODO
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function render() {
+        // TODO
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function translateErrorMessages($statusCode, $errorCode) {
+        // TODO
+    }
+    
+}
