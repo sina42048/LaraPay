60251d8886a7561ea348d3c0a7b8a2967ddbc503
sina42048 <sinafat76@gmail.com>
2021-05-28 10:48:39 +0430

    added new settings for zarinpal driver

--- config/larapay.php
+++ config/larapay.php
@@ -16,5 +16,14 @@ return [
         'payment_request_url' => 'https://api.idpay.ir/v1/payment',
         'payment_verification_url' => 'https://api.idpay.ir/v1/payment/inquiry',
         'sand_box' => false,
+        ],
+    'zarinpal' => [
+        'class' => \Sina42048\LaraPay\Driver\ZarinPal\ZarinPal::class,
+        'merchant_id' => 'xxxxx',
+        'callback_url' => 'http://localhost.dev',
+        'payment_start_url' => 'https://www.zarinpal.com/pg/StartPay',
+        'payment_request_url' => 'https://api.zarinpal.com/pg/v4/payment/request.json',
+        'payment_verify_url' => 'https://api.zarinpal.com/pg/v4/payment/verify.json',
+        'sand_box' => false
     ]
 ];
