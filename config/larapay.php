f77157f156d60de8d17448bb4bd06142b22199f4
sina42048 <sinafat76@gmail.com>
2021-05-28 14:26:35 +0430

    added parspal settings to config

--- config/larapay.php
+++ config/larapay.php
@@ -25,5 +25,16 @@ return [
         'payment_request_url' => 'https://api.zarinpal.com/pg/v4/payment/request.json',
         'payment_verify_url' => 'https://api.zarinpal.com/pg/v4/payment/verify.json',
         'sand_box' => false
+    ],
+    'parspal' => [
+        'class' => \Sina42048\LaraPay\Driver\ParsPal\ParsPal::class,
+        'api_key' => 'xxxxx',
+        'callback_url' => 'http://localhost.dev',
+        'payment_request_url' => 'https://api.parspal.com/v1/payment/request',
+        'payment_verify_url' => ' https://api.parspal.com/v1/payment/verify',
+        'payment_sand_box_request_url' => 'https://sandbox.api.parspal.com/v1/payment/request',
+        'payment_sand_box_verify_url' => ' https://sandbox.api.parspal.com/v1/payment/verify',
+        'sand_box' => false
     ]
+
 ];
